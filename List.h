daecc7f804a333f21259ebe2d1ab84c8da382d53
Carlos Barrientos <barryphotos7@gmail.com>
2022-11-07 15:55:51 -0600

    Adding List to code

--- List.h
+++ List.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <functional>
+template <typename T>
 ref class List
 {
+public:
+    virtual int get_size() = 0;
+
+    virtual bool is_empty() = 0;
+
+    virtual bool is_not_empty() {
+        return !is_empty();
+    }
+
+    virtual void add(T* value) = 0;
+
+    virtual bool insert(int index, T* value) = 0;
+
+    virtual T* get(int index) = 0;
+
+    virtual T* remove(int index) = 0;
 };
 
