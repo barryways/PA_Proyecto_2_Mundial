b503d6599d5a5e4048b2fe4f0ea660af503e4fc5
barryways <93161779+barryways@users.noreply.github.com>
2022-11-17 23:43:54 -0600

    Adding finals results

--- Proyecto2_PA.cpp
+++ Proyecto2_PA.cpp
@@ -29,6 +29,26 @@ static Sticker* search(List<Sticker>* stickers, string code) {
 
 }
 
+static Sticker* search_by_name(List<Sticker>* stickers, string name) {
+    int c = 0;
+    Sticker* temp;
+    while (c != stickers->get_size())
+    {
+        if (c == stickers->get_size())
+        {
+            return nullptr;
+        }
+        temp = stickers->get(c);
+        if (temp->get_code() == name)
+        {
+            return temp;
+        }
+        c++;
+    }
+
+}
+
+
 static int repeated_watcher(List<Sticker>* stickers, Sticker* temp) {
     int contador = 0;
     int repetidas = 0;
@@ -88,6 +108,61 @@ static int marked_watcher(List<Sticker>* stickers, Sticker* temp) {
     return (stickers->get_size() - (marcadas));
 }
 
+static string code_search(List<Sticker>* stickers, Sticker* temp, string code) {
+    try
+    {
+        if (search(stickers, code))
+        {
+            int contador = 0;
+            while (contador != stickers->get_size())
+            {
+                temp = stickers->get(contador);
+                if (temp->get_code() == code)
+                {
+                    string repeated = temp->is_repeated() ? "true" : "false";
+                    string contenido = temp->get_category()->get_name() + "\t" + temp->get_code() + "\t" + temp->get_name() + "\t" + repeated + "\t" + to_string(temp->get_appeareances()) + "\r\n";
+                    return contenido;
+                }
+                contador++;
+            }
+        }
+        else {
+            cout << "No se pudo marcar la estampilla porque no se encontro dentro de la lista" << endl;
+        }
+    }
+    catch (const std::exception&)
+    {
+        cout << "No se pudo completar la operacion solicitada" << endl;
+    }
+}
+
+static string name_search(List<Sticker>* stickers, Sticker* temp, string name) {
+    try
+    {
+        if (search(stickers, name))
+        {
+            int contador = 0;
+            while (contador != stickers->get_size())
+            {
+                temp = stickers->get(contador);
+                if (temp->get_name() == name)
+                {
+                    string repeated = temp->is_repeated() ? "true" : "false";
+                    string contenido = temp->get_category()->get_name() + "\t" + temp->get_code() + "\t" + temp->get_name() + "\t" + repeated + "\t" + to_string(temp->get_appeareances()) + "\r\n";
+                    return contenido;
+                }
+                contador++;
+            }
+        }
+        else {
+            cout << "No se pudo marcar la estampilla porque no se encontro dentro de la lista" << endl;
+        }
+    }
+    catch (const std::exception&)
+    {
+        cout << "No se pudo completar la operacion solicitada" << endl;
+    }
+}
 
 void print_stickers(Sticker * sticker) {
 
@@ -95,6 +170,7 @@ void print_stickers(Sticker * sticker) {
     string linea= sticker->get_category()->get_name() + "\t" + sticker->get_code() + "\t" + sticker->get_name() + "\t" + repeated + "\t" + to_string(sticker->get_appeareances()) + "\r\n";
     cout << linea;
 }
+
 void write_in_file(Sticker* sticker) {
     string repeated = sticker->is_repeated() ? "true" : "false";
     texto_archivo_publico += sticker->get_category()->get_name() + "," + sticker->get_code() + "," + sticker->get_name() + "," + repeated + "," + to_string(sticker->get_appeareances())+"\r";
@@ -120,7 +196,6 @@ void escribir_archivo(string texto_csv) {
     archivo << texto_csv;
     archivo.close();
 }
-
 
 
 void manejo_estadisticas(List<Sticker>* stickers, Sticker* nodo_actual) {
@@ -151,16 +226,18 @@ void manejo_estadisticas(List<Sticker>* stickers, Sticker* nodo_actual) {
     } while (opcion_manejo_estampillas == 0);
 }
 
-
 void manejo_marcaciones_estampillas(List<Sticker>* stickers, Sticker* nodo_principal) {
     int opcion_manejo_estampillas = 0;
+    int opcion_busqueda_estampilla = 0;
     string opcion_marcar_estampilla = "";
     string opcion_desmarcar_estampilla = "";
+    string opcion_buscar_estampilla_nombre = "";
+    string opcion_buscar_estampilla_codigo = "";
 
     cout << "Usted entro a: MARCAR O DESMARCAR ESTAMPILLAS\n\r";
     do
     {
-        cout << "Que desea hacer ? \r\n1.Marcar estampillas\r\n2.Desmarcar estampillas por CSV\r\n3.Ver las estampillas actuales\r\n4.Volver al Menu 1 \r\n";
+        cout << "Que desea hacer ? \r\n1.Marcar estampillas\r\n2.Desmarcar estampillas por CSV\r\n3.Ver las estampillas actuales\r\n4.Buscar Estampa \r\n5.Volver al Menu 1 \r\n";
         cin >> opcion_manejo_estampillas;
         switch (opcion_manejo_estampillas)
         {
@@ -199,24 +276,14 @@ void manejo_marcaciones_estampillas(List<Sticker>* stickers, Sticker* nodo_princ
                 cout << "No se pudo completar la operacion solicitada" << endl;
             }
 
-
-            //cout<< search(stickers, "ARG4")<<"\r\n";//este seria messi
-
-            //if (search(stickers, "FWC2") == nullptr)
-            //{
-            //    cout << "Este no es el dato buscado\r\n";
-            //}
-            //else {
-            //    cout << "Este es el dato buscado\r\n";
-            //    Console::WriteLine( search(stickers, "FWC2"));
-            //}
-
             break;
         case 2:
             try
             {
-                cout << "Que estampilla desea desmarcar? (ponga el codigo unico de estampa)";
+                cout << "Que estampilla desea desmarcar? (ponga el codigo unico de estampa)\r\n";
+
                 cin >> opcion_desmarcar_estampilla;
+
                 if (search(stickers, opcion_desmarcar_estampilla))
                 {
                     int contador = 0;
@@ -240,6 +307,7 @@ void manejo_marcaciones_estampillas(List<Sticker>* stickers, Sticker* nodo_princ
                 }
                 else {
                     cout << "No se pudo desmarcar la estampilla porque no se encontro dentro de la lista" << endl;
+                
                 }
             }
             catch (const std::exception&)
@@ -254,6 +322,92 @@ void manejo_marcaciones_estampillas(List<Sticker>* stickers, Sticker* nodo_princ
             stickers->for_each(print_stickers);
             break;
         case 4:
+            try
+            {
+                cout << "Como desea buscar la estampilla? 1. Codigo Unico 2. Nombre\r\n";
+                
+                cin >> opcion_busqueda_estampilla;
+                do
+                {
+                    switch (opcion_busqueda_estampilla)
+                    {
+                    case 1:
+                        try
+                        {
+                            cout << "Ingrese el codigo unico de estampa" << endl;
+                            cin >> opcion_buscar_estampilla_codigo;
+                            if (search(stickers, opcion_buscar_estampilla_codigo))
+                            {
+                                int contador = 0;
+                                string contenido = "";
+                                while (contador != stickers->get_size())
+                                {
+                                    nodo_principal = stickers->get(contador);
+                                    if (nodo_principal->get_code() == opcion_buscar_estampilla_codigo)
+                                    {
+                                        string repeated = nodo_principal->is_repeated() ? "true" : "false";
+                                        contenido = nodo_principal->get_category()->get_name() + "\t" + nodo_principal->get_code() + "\t" + nodo_principal->get_name() + "\t" + repeated + "\t" + to_string(nodo_principal->get_appeareances()) + "\r\n Este es el codigo de messi \"" + nodo_principal->get_name() + "\"";
+                                        break;
+                                    }
+                                    contador++;
+                                }
+                                cout << contenido << endl;
+                            }
+                            else {
+                                cout << "No se pudo marcar la estampilla porque no se encontro dentro de la lista" << endl;
+                                break;
+                            }
+                        }
+                        catch (const std::exception&)
+                        {
+                            cout << "No se pudo completar la operacion solicitada" << endl;
+                        }
+                        break;
+                    case 2:
+                       /* cout << "Ingrese el nombre del jugador de estampa" << endl;
+                        cin >> opcion_buscar_estampilla_nombre;
+                        try
+                        {
+                            if (search_by_name(stickers, opcion_buscar_estampilla_nombre))
+                            {
+                                int contador2 = 0;
+                                string contenido = "";
+                                while (contador2 != stickers->get_size())
+                                {
+                                    nodo_principal = stickers->get(contador2);
+                                    if (nodo_principal->get_name() == opcion_buscar_estampilla_nombre)
+                                    {
+                                        string repeated = nodo_principal->is_repeated() ? "true" : "false";
+                                        contenido = nodo_principal->get_category()->get_name() + "\t" + nodo_principal->get_code() + "\t" + nodo_principal->get_name() + "\t" + repeated + "\t" + to_string(nodo_principal->get_appeareances()) + "\r\n Este es el codigo de messi \"" + nodo_principal->get_name() + "\"";
+                                        break;
+                                    }
+                                    contador2++;
+                                }
+                                cout << contenido << endl;
+                                break;
+                            }
+                            else {
+                                cout << "No se pudo marcar la estampilla porque no se encontro dentro de la lista" << endl;
+                                break;
+                            }
+                        }
+                        catch (const std::exception&)
+                        {
+                            cout << "No se pudo completar la operacion solicitada" << endl;
+                        }
+                        break;*/
+                    default:
+                        cout << "Esta opcion no la maneja el progrma" << endl;
+                        break;
+                    }
+                } while (opcion_busqueda_estampilla==0);
+            }
+            catch (const std::exception&)
+            {
+                cout << "No se pudo realzar la operacion esperada, contactese con un administrador\r\n";
+            }
+                break;
+        case 5:
                 break;
         default:
             cout << "El programa no maneja esa opcion, pongase en contacto con un administrador\r\n";
